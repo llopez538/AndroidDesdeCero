e1388b8c5e444600c86aa88cbb064c932a323ba0
Leonard López Molina <leonardlopez@Leonards-Mac-mini.local>
2022-04-02 02:54:50 -0400

    LayoutManager Grid de peliculas

--- app/src/main/java/com/misterfront/androiddesdecero/MainActivity.kt
+++ app/src/main/java/com/misterfront/androiddesdecero/MainActivity.kt
@@ -2,14 +2,37 @@ package com.misterfront.androiddesdecero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val movie = findViewById<MovieView>(R.id.movie)
-        movie.setMovie(Movie("Batman", "https://loremflickr.com/320/240"))
+        val recycler = findViewById<RecyclerView>(R.id.recycler)
+
+        val layoutManager = GridLayoutManager(this, 3, GridLayoutManager.VERTICAL, false)
+
+        recycler.layoutManager = layoutManager
+
+        recycler.adapter = MoviesAdapter(listOf(
+            Movie("Movie 1", "https://loremflickr.com/320/240?lock=1"),
+            Movie("Movie 2", "https://loremflickr.com/320/240?lock=2"),
+            Movie("Movie 3", "https://loremflickr.com/320/240?lock=3"),
+            Movie("Movie 4", "https://loremflickr.com/320/240?lock=4"),
+            Movie("Movie 5", "https://loremflickr.com/320/240?lock=5"),
+            Movie("Movie 6", "https://loremflickr.com/320/240?lock=6"),
+            Movie("Movie 7", "https://loremflickr.com/320/240?lock=7"),
+            Movie("Movie 8", "https://loremflickr.com/320/240?lock=8"),
+            Movie("Movie 9", "https://loremflickr.com/320/240?lock=9"),
+
+
+            ))
+
+        // val movie = findViewById<MovieView>(R.id.movie)
+        // movie.setMovie(Movie("Batman", "https://loremflickr.com/320/240"))
     }
 
 }
